da7716cac7926022f8650e505ba0a74b44aae482
ryanlawler1 <122904157+ryanlawler1@users.noreply.github.com>
2023-02-05 23:53:50 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,7 +1,9 @@
 public class Dog extends Enemy{
 
+protected String color;
     public Dog(){
-
         super("Dog", 20, 15, "White","Small");
     }
+    public Dog(String color);
+    this.color = color;
 }
